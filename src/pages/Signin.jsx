3f50bb03c83ce6d595f7a8553b89bf707eea3ef8
MUHAMMADFARHAN1 <Farhan_75230@hotmail.com>
2025-05-15 01:27:08 +0200

    YUP library Integration!

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 import coding from "./../assets/coding.png";
 import { NavLink } from "react-router";
 
+// synchronous and asynchronous YUP (https://strapi.io/blog/yup-validation-in-react-hook-form-a-complete-guide-with-examples)
+// React form Validation with yup (https://dev.to/lawaniej/form-validation-with-yup-2kmg) (https://medium.com/@olivier.trinh/how-to-create-form-validation-with-yup-library-in-reactjs-4846f045957a)
+//React form hook Validation with library (https://dev.to/franciscomendes10866/react-form-validation-with-react-hook-form-and-yup-4a98)
+
+
 export default function Signin() {
   return (
     <div className="grid grid-cols-2 h-screen">
