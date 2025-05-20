435ffff060f2f22bc551a6ff6db401810cfe4eb6
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-21 00:39:13 +0200

    Page not founded made more robust!

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 
+import { useSelector } from "react-redux";
+
 export default function PageNotFound() {
+  const auth = useSelector((store) => store.authenticated);
+  console.log(auth);
   return (
     <>
-      <Navbar />
+      {auth == true ? <Navbar /> : <div></div>}
+      {/* <Navbar /> */}
       <div>
         <p>Sorry, the page could not be located.</p>
       </div>
