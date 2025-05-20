a09e23e00ee1ee3491d7d0987e39c1719eb2ba6f
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-20 23:25:39 +0200

    Navbar component created!

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,31 +1,32 @@
 import React from "react";
 
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { useNavigate, NavLink } from "react-router";
+// import { useSelector } from "react-redux";
+// import { useDispatch } from "react-redux";
+// import { useNavigate, NavLink } from "react-router";
 
-import { LOGIN_USER, LOGOUT_USER } from "./../redux/slice/types";
+// import { LOGIN_USER, LOGOUT_USER } from "./../redux/slice/types";
+import Navbar from "../Components/Navbar";
 
 export default function Dashboard() {
-  const token = localStorage.getItem("token");
-  const dispatch = useDispatch();
-  const navigateTo = useNavigate();
+  // const token = localStorage.getItem("token");
+  // const dispatch = useDispatch();
+  // const navigateTo = useNavigate();
 
-  if (token) {
-    dispatch({ type: LOGIN_USER });
-  }
+  // if (token) {
+  //   dispatch({ type: LOGIN_USER });
+  // }
 
-  const logout = () => {
-    localStorage.removeItem("token");
-    dispatch({ type: LOGOUT_USER });
-    navigateTo("/signin");
-    console.log("Hello");
-  };
+  // const logout = () => {
+  //   localStorage.removeItem("token");
+  //   dispatch({ type: LOGOUT_USER });
+  //   navigateTo("/signin");
+  //   console.log("Hello");
+  // };
 
   return (
-    <div className=" flex flex-row mt-8 mx-18 justify-between">
-      <p>Dashboard</p>
-      <button onClick={logout}>Logout</button>
-    </div>
+    <>
+      <Navbar />
+      <h1>Welcome to the dashboard!</h1>
+    </>
   );
 }
