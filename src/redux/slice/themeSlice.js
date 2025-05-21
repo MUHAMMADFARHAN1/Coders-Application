183374adc469600d78319c39a45027693627cb09
MUHAMMADFARHAN1 <Farhan_75230@hotmail.com>
2025-05-22 01:04:26 +0200

    themeSlice.js erstellen

---
+++ src/redux/slice/themeSlice.js
@@ -0,0 +1,4 @@
+//Theme Toggling can be done in 3 ways
+//1.Use inbuilt features of tailwind
+//2.Use Context API to cause a switch(this can work as well since we have same component tree
+//3. Use Redux to create global theme state (which we will do)
