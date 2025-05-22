b523fc77350f9d2a2f14bb31823a3082a83c6a81
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-22 22:19:46 +0200

    Leader Board Page generated!

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
-
 export default function Dashboard() {
   return (
     <>
       <Navbar />
       <h1>Welcome to the dashboard!</h1>
-      <div className="grid grid-cols-4 ">
-        <p>Rank</p>
-        <p>Name</p>
-        <p>Score</p>
-        <p>Solved Challenges</p>
-      </div>
     </>
   );
 }
--- src/pages/leaderboard.jsx
+++ src/pages/leaderboard.jsx
@@ -1,11 +1,37 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
+import data from "./../api/LeaderData.json";
 
 export default function Leaderboard() {
+  console.log(data);
   return (
     <>
       <Navbar />
-      <h1>Leaderboard</h1>
+      <h1 className="ml-4 mt-4 text-3xl">Leaderboard</h1>
+      <br />
+      <br />
+      <div className="grid grid-cols-5 text-center gap-0">
+        <p>Rank</p>
+        <p>First Name</p>
+        <p>Last Name</p>
+        <p>Score</p>
+        <p>Solved Challenges</p>
+      </div>
+      <br />
+      {data.map((details) => (
+        <>
+          <br />
+          <div className="grid grid-cols-5 text-center gap-0">
+            <p>{details.rank}</p>
+            <p>{details.first_name}</p>
+            <p>{details.last_name}</p>
+            <p>{details.score}</p>
+            <p>{details.solved_challenges}</p>
+            <br />
+          </div>
+          <hr />
+        </>
+      ))}
     </>
   );
 }
