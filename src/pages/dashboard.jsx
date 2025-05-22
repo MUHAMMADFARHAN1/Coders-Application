799069d7969260b2219e039996eef33fed7ea760
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-22 21:27:19 +0200

    Json added alomng with Grid

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,11 +2,16 @@ import React from "react";
 import Navbar from "../Components/Navbar";
 
 export default function Dashboard() {
-
   return (
     <>
       <Navbar />
       <h1>Welcome to the dashboard!</h1>
+      <div className="grid grid-cols-4 ">
+        <p>Rank</p>
+        <p>Name</p>
+        <p>Score</p>
+        <p>Solved Challenges</p>
+      </div>
     </>
   );
 }
